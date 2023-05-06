11ce30cecae3fc5c8169985a2df12c8172bbe341
oleksandr-ishchuk <alexmway@mail.ru>
2023-05-06 20:08:05 +0200

    add autoloader for test.php

--- test.php
+++ test.php
@@ -1,5 +1,10 @@
 <?php
 
-require "https://github.com/dfdxAlex/pattern/blob/main/PHP/Creational%20patterns/Singleton/TestSingleton.php";
+spl_autoload_register(function ($class_name) {
+    $hablon='/[^\d\w]/';
+    $class_name=preg_replace($hablon,DIRECTORY_SEPARATOR,$class_name);
+    require $class_name . '.php';
+  } 
+  );
 
-$obj = \TestSingleton::getInstance("hello");
+$obj = \PHP\CreationalPatterns\Singleton\TestSingleton::getInstance("hello");
