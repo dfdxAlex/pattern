89d1f82fe8d3f906c3b2b15f7c72e7a32b96e052
oleksandr-ishchuk <alexmway@mail.ru>
2023-05-06 23:57:24 +0200

    create SimpleFactory

--- PHP/CreationalPatterns/SimpleFactory/SimpleFactory.php
+++ PHP/CreationalPatterns/SimpleFactory/SimpleFactory.php
@@ -1,2 +1,12 @@
 <?php
 namespace PHP\CreationalPatterns\SimpleFactory;
+
+class SimpleFactory
+{
+    static public function createObject($in)
+    {
+        if ($in == 1) return new Class1;
+        if ($in == 2) return new Class2;
+        if ($in == 3) return new Class3;
+    }
+}
