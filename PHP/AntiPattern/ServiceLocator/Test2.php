9328194f60c66cdd0de1d6490347656ebc84e9f2
oleksandr-ishchuk <alexmway@mail.ru>
2023-06-24 19:54:07 +0200

    add dependency injection

--- PHP/AntiPattern/ServiceLocator/Test2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Test2 implements Service
-{
-    public function getTest()
-    {
-        return "Тест 2";
-    }
-}
--- PHP/AntiPattern/ServiceLocator/Test3.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Test3 implements Service
-{
-    public function getTest()
-    {
-        return "Тест 3";
-    }
-}
