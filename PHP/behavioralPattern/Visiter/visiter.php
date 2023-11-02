15b0c405639bc4971fa8cbe1d70dcd394fc0552f
oleksandr-ishchuk <alexmway@mail.ru>
2023-11-02 19:41:45 +0100

    add psttern visiter

--- PHP/behavioralPattern/Visiter/visiter.php
+++ PHP/behavioralPattern/Visiter/visiter.php
@@ -1,13 +1,5 @@
 <?php
 
-spl_autoload_register(function ($class_name) {
-    $hablon='/[^\d\w]/';
-    $class_name=preg_replace($hablon,DIRECTORY_SEPARATOR,$class_name);
-    require $class_name . '.php';
-  } 
-  );
-
-
 interface IMathPlus
 {
   public function plusAB($property);
