76238a5fbcea3fe864e4289a3987fdc1b8422ffe
oleksandr-ishchuk <alexmway@mail.ru>
2023-12-10 18:19:51 +0100

    add leson for FastRouter

--- src/FastRouter.php
+++ src/FastRouter.php
@@ -1,11 +1,26 @@
 <form>
     <input type="submit" formaction="/pattern/src/FastRouter.php/" value="Home">
-    <input type="submit" formaction="/pattern/src/FastRouter.php/about.php" value="About">
+    <input type="submit" formaction="/pattern/src/FastRouter.php/about.php/aaa" value="About">
     <input type="submit" formaction="/pattern/src/FastRouter.php/contact.php" value="Contact">
     <input type="submit" formaction="/pattern/src/FastRouter.php/messages.php" value="Messages">
 </form>
 
 <?php
+
+define('IS_DEBUG_ENABLED', true);
+
+class Test
+{
+    public function test1()
+    {
+        return 'Возврат test1';
+    }
+    public function test2()
+    {
+        return 'Возврат test2';
+    }
+}
+
 /**
  * Установить FastRouter
  * composer require nikic/fast-route
@@ -14,14 +29,17 @@
 
  require '../vendor/autoload.php';
 
- $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+ $dispatcher = FastRoute\cachedDispatcher(function(FastRoute\RouteCollector $r) {
     $r->addGroup('/pattern/src/FastRouter.php', function (FastRoute\RouteCollector $r) {
-        $r->addRoute('GET', '/', 'home');
-        $r->addRoute('GET', '/about.php', 'about');
+        $r->addRoute('GET', '/', 'Test@test2');
+        $r->addRoute('GET', '/about.php/{name}', 'about');
         $r->addRoute('GET', '/contact.php', 'contact');
         $r->addRoute('GET', '/messages.php', 'messages');
     });
-});
+}, [
+    'cacheFile' => __DIR__ . '/route.cache', /* required */
+    'cacheDisabled' => IS_DEBUG_ENABLED,     /* optional, enabled by default */
+]);
 
 // Fetch method and URI from somewhere
 $httpMethod = $_SERVER['REQUEST_METHOD'];
@@ -52,8 +70,20 @@ switch ($routeInfo[0]) {
         $handler = $routeInfo[1];
         $vars = $routeInfo[2];
 
+
+        $mas = explode('@',$handler);
+        if ($mas[0] == 'Test') {
+            $aaa = new $mas[0];
+            $methode = $mas[1];
+            echo 'Обработка контроллером Test---'.$aaa->$methode(); 
+        }
+
         if ($handler == 'home') {
             echo 'Привет, ты перешел на домашнюю страницу';
+        }
+
+        if ($handler == 'home2') {
+            echo 'Путь из кеша';
         }
 
         if ($handler == 'about') {
